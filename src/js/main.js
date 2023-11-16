54e930464cd85ba683637a607a2d4a5a8309f277
Patrik Arell <patrikarell@gmail.com>
2023-11-16 16:01:33 +0100

    create item class and some new items

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
+import { Item } from "./models/Item";
 import "/src/scss/style.scss";
+
+const salmon = new Item(
+  "Lax",
+  "Kallrökt",
+  "https://brobergsmat.se/wp-content/uploads/2016/11/kallröktlaxw.jpg"
+);
+const chocolate = new Item(
+  "Chocklad",
+  "2 x marabou 200g",
+  "https://www.professionalsecrets.se/uploads/1/3/3/9/133935227/published/shutterchoklad-258868484.jpg?1618923947"
+);
+const meatballs = new Item(
+  "Köttbullar",
+  "1kg svenska",
+  "https://shared.cdn.smp.schibsted.com/v2/images/1bbc6e72-1efb-4846-8465-46f7a29553fe?fit=crop&h=846&w=1225&s=545dd735fabb69929b29483983b5b6d86106a2b8"
+);
+
+
---
+++ src/js/models/Item.js
@@ -0,0 +1,11 @@
+export class UnfinishedItem {
+    name;
+    description;
+    image;
+
+    constructor(name,description,image) {
+        this.name = name;
+        this.description = description;
+        this.image = image;
+    }
+}
